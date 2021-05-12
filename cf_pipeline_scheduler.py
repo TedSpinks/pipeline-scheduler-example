6e1eceadb49cf4d04c8d873534fc9ffc36c29a2e
Ted Spinks <ted.spinks@codefresh.io>
2021-05-11 23:09:12 -0400

    Update codefresh run with --detach

--- cf_pipeline_scheduler.py
+++ cf_pipeline_scheduler.py
@@ -94,7 +94,7 @@ def main():
         env = deployment['env']
         fulllayerpath = deployment['fulllayerpath']
         cmd = "codefresh run"
-        cmd_args = [deploy_pipeline, "-t=" + deploy_trigger, "-b=" + deploy_branch, "-v CLIENT=" + client,
+        cmd_args = [deploy_pipeline, "--detach", "-t=" + deploy_trigger, "-b=" + deploy_branch, "-v CLIENT=" + client,
             "-v REGION=" + region, "-v ENV=" + env, "-v FULLLAYERPATH=" + fulllayerpath]
         output, exit_code = run_cmd(cmd, cmd_args)
         logging.info(output)
